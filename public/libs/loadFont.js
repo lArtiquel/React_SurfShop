7134f1b2548cfa0f21d1b24708799d922ec11a5b
Artyom <artem.tsvirko@mail.ru>
2020-03-21 18:06:52 +0300

    add patched resources

---
+++ public/libs/loadFont.js
@@ -0,0 +1,5 @@
+WebFont.load({
+  google: {
+	families: ["Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic","Oswald:200,300,400,500,600,700","Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Playfair Display:regular,700,900"]
+  }
+});
