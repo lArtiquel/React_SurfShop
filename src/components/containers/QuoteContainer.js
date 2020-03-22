d70cdfd78d2f47b9a50ff711a5d0f8fe852c281e
Artyom <artem.tsvirko@mail.ru>
2020-03-22 21:14:15 +0300

    Readme content added

--- src/components/containers/QuoteContainer.js
+++ src/components/containers/QuoteContainer.js
@@ -6,7 +6,7 @@ export default () => {
     return(
         <article className='container quote w-container'>
             <p className='quote-text' data-ix='float-in-on-scroll'>
-                Surfing is cool!<br/>Probably...<br/>Not quite sure...<br/>Yeah...<br/>This design look kinda thicc. Whaddya think?
+                Surfing is cool!<br/>Probably...<br/>Not quite sure...<br/>Yeah...<br/>This design is lookin' kinda thicc. Whaddya think?
             </p>
             <div className='text-block-2' data-ix='float-in-on-scroll-2'>
                 Artie McPickle
